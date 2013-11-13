f1c4e696c9f676c068329dcad4dc82c2ff6efe09
unknown <s1200522@WS1200522.bc.local>
2013-11-13 14:06:14 +0200

    Made it work! tutorial 2 valmis!

--- opengl10tutorial/src/com/example/opengl10tutorial/MyRenderer.java
+++ opengl10tutorial/src/com/example/opengl10tutorial/MyRenderer.java
@@ -2,14 +2,15 @@ package com.example.opengl10tutorial;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
-
 import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.GLU;
 
 public class MyRenderer implements Renderer {
 
+	Square square = new Square();
+	
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.8f);
+		gl.glClearColor(0.9f, 0.5f, 0.0f, 0.8f);
 		gl.glShadeModel(GL10.GL_SMOOTH);
 		gl.glClearDepthf(1.0f);
 		gl.glEnable(GL10.GL_DEPTH_TEST);
@@ -19,14 +20,20 @@ public class MyRenderer implements Renderer {
 	
 	public void onDrawFrame(GL10 gl) {
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0.0f,0.0f,-10.0f);
+		square.draw(gl);
 	}
 	
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		gl.glViewport(0, 0, width, height);
 		gl.glMatrixMode(GL10.GL_PROJECTION);
 		gl.glLoadIdentity();
-		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 0100.0f);
+		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height,
+						   0.1f, 100.0f);
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
-		gl.glLoadIdentity();
-	}
+		
+	
+	} 
 }
+
--- opengl10tutorial/src/com/example/opengl10tutorial/Tutorial.java
+++ opengl10tutorial/src/com/example/opengl10tutorial/Tutorial.java
@@ -3,17 +3,14 @@ package com.example.opengl10tutorial;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class Tutorial extends Activity {
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		GLSurfaceView view = new GLSurfaceView(this);
 		view.setRenderer(new MyRenderer());
 		setContentView(view);
