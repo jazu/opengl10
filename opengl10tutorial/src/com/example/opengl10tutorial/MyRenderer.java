2f0220ff755a513b294a8ffe72f16a3b0b08233d
unknown <s1200522@WS1200522.bc.local>
2013-11-13 11:34:47 +0200

    opengl 1.0 tutoriaali valmis

---
+++ opengl10tutorial/src/com/example/opengl10tutorial/MyRenderer.java
@@ -0,0 +1,32 @@
+package com.example.opengl10tutorial;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
+
+public class MyRenderer implements Renderer {
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.8f);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glClearDepthf(1.0f);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL10.GL_LEQUAL);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+	}
+	
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+	}
+	
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 0100.0f);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+	}
+}
